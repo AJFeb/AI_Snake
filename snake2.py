9aa535d89ecb1c854c23befaaf1a12d63d0b6a4c
VioletD789 <xdong1@andrew.cmu.edu>
2020-06-04 20:34:30 -0400

    combination of the original code

--- snake2.py
+++ snake2.py
@@ -1,17 +1,68 @@
 import pygame
 from random import randint
+import math
 
+#initialize pygame
 pygame.init()
 
-display = pygame.display.set_mode((400,300))
+#display canvas
+HEIGHT = 400
+WIDTH = 300
+display = pygame.display.set_mode((WIDTH,HEIGHT))
 
+#define colors
+WHITE = (200, 200, 200)
+BLACK = (0, 0, 0)
+
+#define the size of the grid
+GRID_SIZE = 20
+
+#load the snake head
+snakeHead = pygame.image.load("snakeHead.jpg")
+snakeHead = pygame.transform.scale(snakeHead, (GRID_SIZE, GRID_SIZE)) #scale the image to fit in onr grid box
+snakerect = snakeHead.get_rect()
+#set the initial snake head position in the middle of the canvas
+snakeRow = math.floor(HEIGHT/GRID_SIZE/2)*GRID_SIZE
+snakeCol = math.floor(WIDTH/GRID_SIZE/2)*GRID_SIZE
+
+
+#draw the grid by looping through the whole canvas and draw small squares
+def drawGrid():
+	for x in range(WIDTH):
+		for y in range(HEIGHT):
+			rect = pygame.Rect(x*GRID_SIZE, y*GRID_SIZE,GRID_SIZE, GRID_SIZE)
+			pygame.draw.rect(display, WHITE, rect, 1)
+
+def moveSnakeHead(key, grid_size, snake_row, snake_col):
+	if key == pygame.K_LEFT:
+		snake_col -= grid_size
+	if key == pygame.K_RIGHT:
+		snake_col += grid_size
+	if key == pygame.K_UP:
+		snake_row -= grid_size
+	if key == pygame.K_DOWN:
+		snake_row += grid_size
+	return (snake_row, snake_col)
+
+#Infinite loop that keeps the program running
 stop = False
 while stop == False:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            stop = True
-    pygame.draw.rect(display, (255,255,255), [200,150,10,10])
-    pygame.display.update()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			stop = True
+		if event.type == pygame.KEYDOWN: #if a key is pressed, the snake picture is updated 
+			key = event.key
+			snakeRow, snakeCol = moveSnakeHead(key, GRID_SIZE, snakeRow, snakeCol)
+	pygame.display.update() 
+	 '''fill the canvas with black. In pygame, 
+	 the canvas is redraw after each loop so in order to draw the 
+	 new picture the original picture needs to be coverd by black first,
+	 otherwise, when the new snake head is drawn the previous snake head is still on the canvas
+	 '''
+	display.fill(BLACK)
+	drawGrid() #draw the grid
+	display.blit(snakeHead,(snakeCol,snakeRow)) #snake is placed column first and then row 
+	pygame.display.flip() #update the canvas
 pygame.quit()
 quit()
 
@@ -23,7 +74,7 @@ class Game:
 	def function():
 		pass
 
-class Movements:
+class Movements(keyPress):
 	#
 	x = 10
 	y = 10
@@ -38,8 +89,8 @@ class Movements:
 	def left(self):
 		self.x -= self.speed
 
+
 class App:
 	pass
-	
 
 #App().run()
